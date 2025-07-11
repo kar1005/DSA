dc446944f92b8d498fb64ffeca12d9ecddc6c251
Khushi <khushiruparelia2004@gmail.com>
2025-07-11 08:57:51 +0530

    Todays gfg potd

---
+++ GFG POTD/DP/TrailOfOnes.java
@@ -0,0 +1,22 @@
+class TrailOfOnes {
+    public int countConsec(int n) {
+        
+        if(n==2){
+            return 1;
+        }
+        
+        int first = 0;
+        int second = 1;
+        int third;
+        int result = 1;
+        
+        for(int i=3;i<=n;i++){
+            third = first + second;
+            
+            result = result * 2 + third;
+            first = second;
+            second = third;
+        }
+        return result;
+    }
+}
